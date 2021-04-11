4b0144bc76418281e46ec58f0650fa74718bfc77
naaoufal <naoufelbenmensour@gmail.com>
2021-04-11 23:13:17 +0000

    add files

---
+++ src/components/addOne.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <input name="test" v-model="test" />
+      <button @click="submit" >click</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return {
+            test: ""
+        }
+    },
+    methods: {
+        submit: function() {
+            console.log(this.test)
+            this.test = ""
+        },
+        clearIpnut : function () {
+            this.test = ""
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <addOne />
   </div>
 </template>
 
 <script>
+import addOne from '@/components/addOne'
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    addOne,
   },
 };
 </script>
